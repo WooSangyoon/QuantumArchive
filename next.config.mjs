701012f64fd56cdd156e7f1846f6cb00332dbf14
WooSangyoon <wsy04018@naver.com>
2025-09-26 14:02:48 +0900

    build setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://github.com/WooSangyoon/QuantumArchive"
-      : "",
-};
-
 export default nextConfig
