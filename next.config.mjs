903d2e15d0d8f24757a8c118108ff260e03ccbb8
WooSangyoon <wsy04018@naver.com>
2025-09-26 15:32:40 +0900

    build setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // ← 정적 export로 쓸 거면 남겨두세요
-  images: {
-    unoptimized: true, // Vercel은 최적화 서버 지원하지만, export라면 필요
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+export default nextConfig
